ff49dbeabe6c27420d89648268607b53147cdf6c
bajpai244 <bajpaiharsh244@gmail.com>
2021-07-28 01:51:22 +0530

    add(tap): add notes/programs on TAP specifications

---
+++ js_testing/tape/test.js
@@ -0,0 +1,21 @@
+const test = require("tape");
+
+/**
+ *test(test_name,arg)
+ * test_name is going to be the name of test, arg will have all the methods required to carry on the assertions
+ * */
+
+test("test 1", (t) => {
+  //t.plan(1); // it decalares that 4 assertions have to run
+  t.equal(1, 2);
+  t.equal(1, 1);
+  t.end();
+
+  // t.end(); // it decalares that the testing has ended, not required if you have test.plan()
+});
+
+test("test 2", (t) => {
+  t.ok(true, "the value is trusty");
+
+  t.end();
+});
